665d891186a680cfa8645b7946b4956e70fda3d1
Ymox <ymox@hotmail.ch>
2017-06-26 13:24:06 +0200

    Ajoute le CRUD pour les instrumentations

---
+++ src/YSoft/InstrumBundle/Form/InstrumentationType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace YSoft\InstrumBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InstrumentationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('note');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'YSoft\InstrumBundle\Entity\Instrumentation'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'ysoft_instrumbundle_instrumentation';
+    }
+
+
+}
