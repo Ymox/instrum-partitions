9b62a66d7e0b732377080dec14dc6f441d7d0ff4
Ymox <ymox@hotmail.ch>
2017-06-27 09:37:56 +0200

    Ajoute la couche de sécurité

---
+++ src/YSoft/InstrumBundle/Controller/SecurityController.php
@@ -0,0 +1,23 @@
+<?php
+namespace YSoft\InstrumBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    public function loginAction(Request $request, AuthenticationUtils $authUtils)
+    {
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('login.html.twig', array(
+            'lastUsername' => $lastUsername,
+            'error'        => $error,
+        ));
+    }
+}
