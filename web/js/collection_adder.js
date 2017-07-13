b61c90c8f78d8b80361630b245ad89f2bf575eef
Ymox <ymox@hotmail.ch>
2017-07-13 12:24:56 +0200

    Un peu de styles pour les collections
    Les seules collections actuelles sont les manques. Utilisation des cards, suppression du label pour l'élément de collection (on ne garde que celui de l'unique champ), gestion des boutons de suppression

--- web/js/collection_adder.js
+++ web/js/collection_adder.js
@@ -2,6 +2,9 @@ $(document).ready( function() {
 	$('[data-prototype]').each( function() {
 		var prototype = $(this).data('prototype');
 		$(this).prev().append(' ').append($('<button>', {type: 'button', 'class': 'btn btn-sm btn-success adder', 'html': '<i class="fa fa-plus-circle"></i>'}));
+		$('>div, >tbody>tr', this).filter(function() {
+	    	return $('>.remover', this).length === 0;
+	    }).css('position', 'relative').append('<button class="btn btn-sm btn-danger remover" style="position: absolute; top: 2px; right: 2px;">✖</button>')
 	});
 	$('form').on('click', '.adder', function() {
 	    var $container = $(this).closest('div').find('[data-prototype]');
@@ -11,7 +14,7 @@ $(document).ready( function() {
 	    $container.append(prototype);
 	    $('>div, >tbody>tr', $container).filter(function() {
 	    	return $('>.remover', this).length === 0;
-	    }).css('position', 'relative').append('<button class="btn btn-sm btn-danger remover" style="position: absolute; top: 0px; right: 0px;">✖</button>')
+	    }).css('position', 'relative').append('<button class="btn btn-sm btn-danger remover" style="position: absolute; top: 2px; right: 2px;">✖</button>')
 	});
 	$('form').on('click', '.remover', function(e) {
 		$(e.target).closest('div').remove();
