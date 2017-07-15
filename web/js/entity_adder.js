fbf04e92b64ab276f68da75e26b710da88703ce8
Ymox <ymox@hotmail.ch>
2017-07-15 03:21:18 +0200

    Change le bouton d'ajout d'entités en sa version outline
    Parce que je sens que les personnes voudront plus facilement cliquer dessus si elles le voient "plein"

--- web/js/entity_adder.js
+++ web/js/entity_adder.js
@@ -5,7 +5,7 @@ $(document).ready( function() {
 	$('select.addable').each( function() {
 		var uri = $(this).data('uri');
 		$(this).prev().append(' ').append(
-			$('<a>', {href: uri || '#', 'data-target': '#Modal', 'data-toggle': 'modal', 'html': '<i class="fa fa-plus-circle"></i>', target: '_blank', 'class': 'btn btn-sm btn-success'})
+			$('<a>', {href: uri || '#', 'data-target': '#Modal', 'data-toggle': 'modal', 'html': '<i class="fa fa-plus-circle"></i>', target: '_blank', 'class': 'btn btn-sm btn-outline-success'})
 		);
 	});
 	$(document).on('show.bs.modal', function(e) {
