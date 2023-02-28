98e4d5a6d56ad22209d200d8fa958fb2cd07dd40
Ymox <ymox@hotmail.ch>
2023-02-28 12:54:22 +0100

    Gestion des initiales des prénoms
    Selon les règles françaises

--- src/Twig/TwigName.php
+++ src/Twig/TwigName.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Twig;
 
+use App\Entity\Person;
 use Twig\Extension\AbstractExtension;
 
 class TwigName extends AbstractExtension
@@ -8,13 +9,34 @@ class TwigName extends AbstractExtension
     public function getFilters(): array
     {
         return [
-            new \Twig\TwigFunction('initialism', [$this, 'getInitialism'], ['is_safe' => ['html']]),
+            new \Twig\TwigFilter('initialism', [$this, 'getInitialism'], ['is_safe' => ['html']]),
         ];
     }
 
-    public function getInitialism(?string $name): ?string
+    public function getInitialism($name): ?string
     {
-        preg_match($name, );
-        return $name;
+        if (is_string($name)) {
+            return $this->initialize($name);
+        } else if (!($name instanceof Person)) {
+            throw new \UnexpectedValueException(sprintf('Expected argument of type string, \Traversable or \App\Entity\Person, got "%s"', get_debug_type($name)));
+        }
+
+        if ($name->getFirstName()) {
+            $result = $this->initialize($name->getFirstName()) . ' ' . $name->getLastName();
+        } else {
+            $result = (string) $name;
+        }
+
+        return $result;
+    }
+
+    private function initialize(string $name): ?string
+    {
+        if (!$name) {
+            return null;
+        }
+
+        $initials = preg_replace('`(?<=^|\W)(?<initial>[AEIOUY]|[^AEIOUY][^aeiouy\']*)[^ \'-]+`', '$1.', $name);
+        return '<abbr title="' . $name . '">' . ($initials ?? $name) . '</abbr>';
     }
 }
