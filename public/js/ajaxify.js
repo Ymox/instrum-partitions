f8d3da666b4ebc2b7c37e6b4dd8526affc8eebb0
Ymox <ymox@hotmail.ch>
2023-08-16 22:53:11 +0200

    Script "d'ajaxification" basé sur les Promise
    Permet de gérer les retours en erreur sans utiliser $.ajax

--- public/js/ajaxify.js
+++ public/js/ajaxify.js
@@ -1,21 +1,28 @@
 $(function() {
 	$(document).on('click', 'a.ajaxify', function(e) {
-		$(this.dataset.loadTarget).load(this.href, null, function() {
-			$(document).trigger('ajaxComplete.pulse');
-		});
+		$(this.dataset.loadTarget).load(this.href);
 		e.preventDefault();
 	});
 	$(document).on('submit', 'form.ajaxify', function(e) {
 		var postData = $(this).serializeArray();
 		if (!this.dataset.loadTarget) {
-			$.post(this.action, postData, function(data, stringStatus, jqXHR) {
-				if (data.value && data.text) {
-					$($('#Modal').data('fieldTarget')).append(new Option(data.text, data.value, true, true)).trigger('change');
-					$('#Modal').modal('hide');
-				} else {
-					$('.modal-body').empty().append($('form', data));
+			$.ajax({
+				url: this.action,
+				method: this.method,
+				data: postData
+			 }).then(
+				function(data, stringStatus, jqXHR) {
+					if (data.value && data.text) {
+						$($('#Modal').data('fieldTarget')).append(new Option(data.text, data.value, true, true)).trigger('change');
+						$('#Modal').modal('hide');
+					} else {
+						$('.modal-body').empty().append($('form', data));
+					}
+				},
+				function(jqXHR, stringStatus, errorThrown) {
+					$('.modal-body').empty().append($('form', jqXHR.responseText));
 				}
-			});
+			);
 		} else {
 			$(this.dataset.loadTarget).load(
 				this.action,
